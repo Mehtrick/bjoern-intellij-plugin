e15177077a367af227dae94c07ce00f11644ff5b
mehtrick <p.lauhof@gmail.com>
2025-09-20 14:24:28 +0200

    Migrate build configuration to Kotlin DSL and update IntelliJ Platform version

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.7.1"
+}
+
+group = "de.mehtrick"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        create("IC", "2025.1.4.1")
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+
+        // Add necessary plugin dependencies for compilation here, example:
+        // bundledPlugin("com.intellij.java")
+        bundledPlugin("org.jetbrains.plugins.yaml")
+
+        pluginVerifier()
+        zipSigner()
+    }
+}
+
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+    }
+}
+
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "251"
+        }
+
+        changeNotes = """
+            Initial version
+        """.trimIndent()
+    }
+
+    publishing {
+        token = providers.environmentVariable("PUBLISH_TOKEN")
+    }
+}
